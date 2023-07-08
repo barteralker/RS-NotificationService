3d1375a47900a3b4c2dcd2933745b65962afa2cf
Ramiz Siddiqui <ramiz.siddiqui@gosaas.io>
2023-07-09 01:27:48 +0500

    Adding Queries files

---
+++ PostgresQueries/event.js
@@ -0,0 +1,18 @@
+
+const GET_ALL_EVENTS = "";
+
+const GET_EVENT_BY_ID = "";
+
+const CREATE_EVENT = "";
+
+const UPDATE_EVENT = "";
+
+const DELETE_EVENT = "";
+
+module.exports = {
+    GET_ALL_EVENTS,
+    GET_EVENT_BY_ID,
+    CREATE_EVENT,
+    UPDATE_EVENT,
+    DELETE_EVENT
+}
---
+++ PostgresQueries/message.js
@@ -0,0 +1,18 @@
+
+const GET_ALL_MESSAGES = "";
+
+const GET_MESSAGE_BY_ID = "";
+
+const CREATE_MESSAGE = "";
+
+const UPDATE_MESSAGE = "";
+
+const DELETE_MESSAGE = "";
+
+module.exports = {
+    GET_ALL_MESSAGES,
+    GET_MESSAGE_BY_ID,
+    CREATE_MESSAGE,
+    UPDATE_MESSAGE,
+    DELETE_MESSAGE
+}
