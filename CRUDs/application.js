4dbcd40118c0f230d568c5bb8ca768a7c2b154ca
Ramiz Siddiqui <ramiz.siddiqui@gosaas.io>
2023-07-09 03:23:26 +0500

    Applications CRUD refactored

--- CRUDs/application.js
+++ CRUDs/application.js
@@ -4,27 +4,6 @@ const routerApp = express.Router();
 routerApp.use(express.json());
 const applicationController = require('../Controllers/application');
 
-const Joi = require('joi');
-
-const applications = [
-    {
-        id: 1,
-        name: "ETS",
-        description: "Employee Training System"
-    }
-];
-
-function validateApp(body) {
-
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        description: Joi.string()
-    });
-
-    return schema.validate(body);
-
-};
-
 routerApp.get('/', function(req, res){
 
     ( async() => {
