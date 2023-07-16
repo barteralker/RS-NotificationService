0699e77e2fad5b177a25b6380de7a86f78435d3c
Ramiz Siddiqui <ramiz.siddiqui@gosaas.io>
2023-07-16 22:01:22 +0500

    Async Error handling

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const debug = require('debug')('app:appDebugger');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+require('express-async-errors');
 
 app.use(express.json());
-app.use(morgan('tiny'));
+// app.use(morgan('tiny'));
 
 const Joi = require('joi');
 
@@ -14,14 +15,18 @@ const event = require('./CRUDs/event.js');
 const notification = require('./CRUDs/notification.js');
 const message = require('./CRUDs/message.js');
 
+const errorHandler = require('./Middleware/ErrorHandler.js')
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Notification Service');
+})
+
 app.use('/applications', application);
 app.use('/events', event);
 app.use('/notifications', notification);
 app.use('/messages', message);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Notification Service');
-})
+app.use(errorHandler);
 
 const DB_Conn = require('./resources/config.json').DB_CONN;
 debug(`Connecting to ${DB_Conn} DB..!!`)
