12afc71ac7b5536c968b69fd35330400b9b5af14
Ramiz Siddiqui <ramiz.siddiqui@gosaas.io>
2023-07-17 00:25:14 +0500

    setting up looging module

--- index.js
+++ index.js
@@ -5,21 +5,7 @@ const winston = require('winston');
 
 require('express-async-errors');
 require('./Startup/middlewareSetup')(app);
-
-winston.add(new winston.transports.File({ filename: './logs/logs.log' }));
-
-process.on('uncaughtException', (exp) => {
-    winston.error('UNCAUGHT EXCEPTION');
-    winston.error(exp.message, exp);
-});
-
-process.on('unhandledRejection', (exp) => {
-    winston.error('UNHADLED PROMISE REJECTION');
-    winston.error(exp.message, exp);
-});
-
-const DB_Conn = require('./resources/config.json').DB_CONN;
-winston.info(`Connecting to ${DB_Conn} DB..!!, ${new Date().toUTCString()}`)
+require('./Startup/loggingSetup')(winston);
 
 const port = process.env.port || require('./resources/config.json').port;
 app.listen(port, () => winston.info(`Listening on Port ${port} !`));
