9dc831d0a96ea8061baef64eaeb0eaa927ea2491
Ramiz Siddiqui <ramiz.siddiqui@gosaas.io>
2023-07-17 00:16:18 +0500

    imports added in middlewareSetup.js

--- Startup/middlewareSetup.js
+++ Startup/middlewareSetup.js
@@ -1,6 +1,15 @@
 
+const express = require('express');
+
+const application = require('./CRUDs/application.js');
+const event = require('./CRUDs/event.js');
+const notification = require('./CRUDs/notification.js');
+const message = require('./CRUDs/message.js');
+
+const errorHandler = require('./Middleware/ErrorHandler.js')
+
 module.exports = (app) => {
-    
+
     app.use(express.json());
 
     app.get('/', (req, res) => {
