ed5e4be9427b74a97b814af993c98240ab206e46
Ramiz Siddiqui <ramiz.siddiqui@gosaas.io>
2023-07-20 01:01:16 +0500

    renewing logs file

--- Startup/loggingSetup.js
+++ Startup/loggingSetup.js
@@ -1,7 +1,9 @@
+// import { unlink } from 'node:fs';
+const fs = require('fs');
 
 module.exports = (winston) => {
 
-    winston.add(new winston.transports.File({ filename: './logs/logs.log' }));
+    winston.add(new winston.transports.File({ filename: './logs/logs.log' , options: { flags: 'w' } }));
 
     process.on('uncaughtException', (exp) => {
         winston.error('UNCAUGHT EXCEPTION');
