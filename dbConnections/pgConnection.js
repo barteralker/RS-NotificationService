320e939cbd1eee1d940d9e04a897b1a9ba751ddc
Ramiz Siddiqui <ramiz.siddiqui@gosaas.io>
2023-08-02 12:14:03 +0500

    moving PG password to custom env vars

--- dbConnections/pgConnection.js
+++ dbConnections/pgConnection.js
@@ -1,6 +1,9 @@
 
 const { Pool, Client } = require("pg");
+const config = require('config');
 const credentials = require('../config/default.json').PG_DB_credentials;
+
+credentials.password = config.get('password');
 
 module.exports = {
     Pool,
